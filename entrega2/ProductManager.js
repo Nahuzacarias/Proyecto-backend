9e853d0d06b15c9af8a4c837f359eef20dcf5ff8
Nahuzacarias <zacarias.nahu@gmail.com>
2023-04-01 11:57:24 -0300

    asd

--- entrega2/ProductManager.js
+++ entrega2/ProductManager.js
@@ -1,46 +1,132 @@
-import fs from 'fs';
-
-
-const path = './Productos.json'
+import fs from "fs";
 
 export default class ProductManager {
-
-    consultarProductos = async () => {
-
-        if (fs.existsSync(path)) {
-
-            const data = await fs.promises.readFile(path, 'utf-8')
-
-            const prod = JSON.parse(data);
-            return prod;
-        } else {
-
-
-            return []
-        }
-    }
-    addProduct = async (pro) => {
-
-
-        const producto2 = await this.consultarProductos();
-
-        if (producto2.length === 0) {
-            pro.id = 1
-
-        } else {
-
-            pro.id = producto2[producto2.length-1].id+1;
-        }
-
-        producto2.push(pro);
-
-await fs.promises.writeFile(path, JSON.stringify(producto2,null,'\t'))
-
-return pro
-
-
-
+    constructor() {
+        this.path = "./entrega2/productos.json";
     }
 
+    getProducts = async () => {
+        try {
+            if (fs.existsSync(this.path)) {
+                const data = await fs.promises.readFile(this.path, "utf8");
+                const products = JSON.parse(data);
+                return products
+            } else {
+                return [];
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
+    addProduct = async (title, description, price, thumbnail, code, stock) => {
+        try {
+            const products = await this.getProducts();
+            let producto = {
+                title: title,
+                description: description,
+                price: price,
+                thumbnail: thumbnail,
+                code: code,
+                stock: stock,
+            };
+            let id_producto = producto.id;
+            if (products.length === 0) {
+                producto.id = 1;
+            } else {
+                producto.id = products[products.length - 1].id + 1;
+            }
+            if (
+                Object.values(producto).includes(" ") ||
+                Object.values(producto).includes("")
+            ) {
+                return console.log("Todos los campos son obligatorios");
+            }
+            let codigo = products.find((ele) => ele.code == producto.code);
+            if (codigo) {
+                return console.log(
+                    "El 'code' del producto ya existe, intente cambiarlo."
+                );
+            } else {
+                products.push(producto);
+                await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t"));
+                console.log(producto);
+                return producto;
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    getProductsById = async (id_producto) => {
+        try {
+            const products = await this.getProducts();
+            const producto = products.find((producto) => producto.id == id_producto);
+            if (producto) {
+                return console.log(producto);
+            } else {
+                return "not found";
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    deleteProduct = async (id_producto) => {
+        try {
+            const products = await this.getProducts();
+            const indexProducto = products.findIndex(producto => producto.id == id_producto);
+            if (indexProducto !== -1) {
+                const deleteP = products.splice(indexProducto, 1)[0];
+                await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t")
+                );
+                console.log("El producto fue eliminado");
+                return deleteP
+            } else {
+                return "El producto que quiere eliminar no existe";
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    updateProduct = async (id_producto, title, description, price, thumbnail, code, stock) => {
+        try {
+            let producto = {
+                title: title,
+                description: description,
+                price: price,
+                thumbnail: thumbnail,
+                code: code,
+                stock: stock,
+                id: id_producto
+            };
+            const products = await this.getProducts();
+            const indexProducto = products.findIndex(producto => producto.id === id_producto);
+            if (indexProducto !== -1) {
+                if (Object.values(producto).includes(" ") ||
+                    Object.values(producto).includes("")) {
+                    return console.log("Todos los campos son obligatorios");
+                }
+                let codigo = products.find((ele) => ele.code == producto.code);
+                if (codigo) {
+                    return console.log(
+                        "El 'code' del producto ya existe, intente cambiarlo."
+                    );
+                } else {
+                    const deleteP = products.splice(indexProducto, 1)[0];
+                    await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t"));
+                    deleteP;
+                    products.push(producto);
+                    await fs.promises.writeFile(this.path, JSON.stringify(products, null, "\t"));
+                    console.log("El producto se modificó con exito")
+                    return producto;
+                }
+            } else {
+                return "El producto que quiere modificar no existe"
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
--- entrega2/index.js
+++ entrega2/index.js
@@ -1,25 +1,18 @@
 import ProductManager from "./ProductManager.js";
 
-const productos = new ProductManager();
+const manager = new ProductManager();
 
+const env = async() =>{    
+    
+    //let carga = await manager.addProduct("ryzen", "ryzen 7 ", 82900,"no imagen", 1, 20);
+    
+    //let productos = await manager.getProducts(); console.log(productos)
+    
+    //let id = await manager.getProductsById(3); console.log(id)
+    
+    //let borrar = await manager.deleteProduct(4); console.log(borrar)
 
-const env = async () => {
-
-    let producto = {
-        title: "notebook",
-        description: "i3",
-        price: "210000",
-        thumbnail: "thumbnail",
-        code: "1",
-        stock: "5",
-    }
-
-    let result = await productos.addProduct(producto);
-
-    console.log(result)
-
-let produc = await productos.consultarProductos()
-console.log (produc)
-
+    let modificar = await manager.updateProduct(5, "ryzen","ryzen3", 10, "sin imagen",4,10); console.log(modificar)
 }
+
 env()
