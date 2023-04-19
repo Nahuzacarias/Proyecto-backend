6ecbf68e3b7376e0ecbba84fb40679b78a801844
Nahuzacarias <zacarias.nahu@gmail.com>
2023-04-19 20:56:03 -0300

    v8

--- src/app.js
+++ src/app.js
@@ -1,18 +1,33 @@
 import express from "express"
 import productosRouter from "./routes/productos.js"
 import carroRouter from "./routes/carrito.js"
+import __dirname from "./utils.js";
+import handlebars from "express-handlebars";
+import {Server} from "socket.io";
+import viewRouter from "/components/carritosManager.js"
 
-const PORT = 8080;
+const PORT =  process.env.PORT || 8080;
 
 const app = express();
+const server = app.listen (PORT,()=>{
+
+    console.log ('servidor funcionando en el puerto: ' + PORT )
+})
+
+app.engine('handlebars',handlebars.engine());
+app.set('views', __dirname+'/views');
+app.set('view engine' , 'handlebars');
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname+ '/public'))
+app.use ('/' , viewRouter)
 
-app.listen(PORT, () => {
+const io = new Server(server);
 
-    console.log("server up " + PORT)
+io.on ('connection',socket => {
 
+console.log ('Socket connected');
 
 })
 
